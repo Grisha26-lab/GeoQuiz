d0852612fc183ee26a5fed2192a48b26079152ff
User <grigoriiavanesov@gmail.com>
2022-01-01 13:35:39 +0300

    v1.5

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,14 +2,27 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
+
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProvider(this).get(QuizViewModel::class.java)
+    }
 
 
     private lateinit var trueButton: Button
@@ -17,28 +30,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton: ImageButton
     private lateinit var backButton: ImageButton
     private lateinit var questionTextView: TextView
-    private var currentIndex = 0
     private var currentResult = 0
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true),
-        Question(R.string.question_america_money,false),
-        Question(R.string.question_russia_money,false),
-        Question(R.string.question_russia,true),
-        Question(R.string.question_armenia,true)
-
-
-    )
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(TAG,"onSaveInstanceState")
+        savedInstanceState?.putInt(KEY_INDEX,quizViewModel.currentIndex)
         setContentView(R.layout.activity_main)
-
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -48,41 +47,34 @@ class MainActivity : AppCompatActivity() {
 
         trueButton.setOnClickListener {
             checkAnswer(true)
-            nextQuestion()
+
         }
         falseButton.setOnClickListener {
             checkAnswer(false)
-            nextQuestion()
+
 
         }
         nextButton.setOnClickListener {
-            if (currentIndex == questionBank.size - 1) {
-                currentIndex = 0
-                updateQuestion()
-            }
-            nextQuestion()
+            quizViewModel.moveToNext()
+            updateQuestion()
+
+
         }
 
-        backButton.setOnClickListener {
-            if (currentIndex == 0) {
-                currentIndex = questionBank.size - 1
-                updateQuestion()
-            }
-            backQuestion()
-        }
+
 
         updateQuestion()
     }
 
     private fun updateQuestion() {
-        val questionTextRestId = questionBank[currentIndex].textResId
+        val questionTextRestId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextRestId)
 
 
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
         val messageAnswer = if (userAnswer == correctAnswer) {
             currentResult += 10
             R.string.correct_toast
@@ -93,13 +85,5 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, messageAnswer, Toast.LENGTH_SHORT).show()
     }
 
-    private fun nextQuestion() {
-        currentIndex = (currentIndex + 1) % questionBank.size
-        updateQuestion()
-    }
 
-    private fun backQuestion() {
-        currentIndex = (currentIndex - 1) % questionBank.size
-        updateQuestion()
-    }
 }
---
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.geoquiz
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel : ViewModel() {
+
+    var currentIndex = 0
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true),
+        Question(R.string.question_america_money, false),
+        Question(R.string.question_russia_money, false),
+        Question(R.string.question_russia, true),
+        Question(R.string.question_armenia, true)
+    )
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
+}
