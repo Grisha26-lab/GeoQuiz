d2563c00b2f7c9ec0fbdbf657348e91db043635f
user <grisha2401>
2022-02-21 00:51:28 +0300

    Добавил второе активити

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.example.geoquiz
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import java.lang.Exception
 
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
@@ -64,7 +61,8 @@ class MainActivity : AppCompatActivity() {
 
         }
         cheatButton.setOnClickListener {
-            val intent = Intent(this, CheatActivity::class.java)
+            val answerTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerTrue)
             startActivity(intent)
         }
 
